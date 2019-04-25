2599c6d7d2afa61f3ae1ffd7394b830bdf9c68ac
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2019-04-25 13:26:24 -0500

    First Commit

---
+++ src/extensions/spFxExtensions/SpFxExtensionsApplicationCustomizer.ts
@@ -0,0 +1,39 @@
+import { override } from '@microsoft/decorators';
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer
+} from '@microsoft/sp-application-base';
+import { Dialog } from '@microsoft/sp-dialog';
+
+import * as strings from 'SpFxExtensionsApplicationCustomizerStrings';
+
+const LOG_SOURCE: string = 'SpFxExtensionsApplicationCustomizer';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface ISpFxExtensionsApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  testMessage: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class SpFxExtensionsApplicationCustomizer
+  extends BaseApplicationCustomizer<ISpFxExtensionsApplicationCustomizerProperties> {
+
+  @override
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+    let message: string = this.properties.testMessage;
+    if (!message) {
+      message = '(No properties were provided.)';
+    }
+
+    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
+
+    return Promise.resolve();
+  }
+}
---
+++ src/extensions/spFxExtensions/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ISpFxExtensionsApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'SpFxExtensionsApplicationCustomizerStrings' {
+  const strings: ISpFxExtensionsApplicationCustomizerStrings;
+  export = strings;
+}
