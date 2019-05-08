ac1a0dd0ed47e9776a45ef7ea29928aaee8d2c7a
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2019-05-08 09:06:19 -0500

    Handling mobile app secondary loads

--- src/extensions/spFxExtensions/spfxFooter/SpfxFooterApplicationCustomizer.ts
+++ src/extensions/spFxExtensions/spfxFooter/SpfxFooterApplicationCustomizer.ts
@@ -33,6 +33,8 @@ export default class SpfxFooterApplicationCustomizer
       this.properties.context = props.context;
     }
   
+    private _loadCount: number = 0;
+
   private _bottomPlaceholder: PlaceholderContent | undefined;
 
   private _footerText: string = `<footer id="spfx-footer" class="${styles.app}">
@@ -45,10 +47,12 @@ export default class SpfxFooterApplicationCustomizer
 
   private _renderPlaceHolders(): void {
     // Handling the bottom placeholder
+
     if (!this._bottomPlaceholder) {
       this._bottomPlaceholder = this.properties.context["placeholderProvider"].tryCreateContent(
         PlaceholderName.Bottom,
-        { onDispose: this._onDispose }
+        // { onDispose: this._onDispose }
+        { onDispose: () => { this._onDispose(); } }
       );
 
       // The extension should not assume that the expected placeholder is available.
@@ -68,6 +72,8 @@ export default class SpfxFooterApplicationCustomizer
   }
 
   private _onDispose(): void {
+    console.log(this);
+    this._bottomPlaceholder = null;
     console.log('[SpfxFooterApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
   }
 
@@ -84,17 +90,33 @@ export default class SpfxFooterApplicationCustomizer
   }
 
   public initExtension(): Promise<void> {
+    // Log.info(LOG_SOURCE, `Initialized SPFx Footer`);
+    //   addStyleSheetRules();
+    //   checkForElemExistance();
+    //   handlePrint();
+      
+      // this._defineRemove();
+    // if (false) {
+      if (this._loadCount === 0) {
+      Log.info(LOG_SOURCE, `Initialized SPFx Footer`);
+      addStyleSheetRules();
+      checkForElemExistance();
+      handlePrint();
+      
+      this._defineRemove();
+  
+      // Wait for the placeholders to be created (or handle them being changed) and then
+      // render.
+      
+      this.properties.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
+    } else if (this._loadCount === 1) {
+      let elem = document.createElement('div');
+      elem.setAttribute('style', 'position: fixed; bottom: 0; width: 100%; z-index: 999;');
+      elem.innerHTML = this._footerText;
 
-    Log.info(LOG_SOURCE, `Initialized SPFx Footer`);
-    addStyleSheetRules();
-    checkForElemExistance();
-    handlePrint();
-    
-    this._defineRemove();
-
-    // Wait for the placeholders to be created (or handle them being changed) and then
-    // render.
-    this.properties.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
+      document.body.appendChild(elem);
+    }
+    this._loadCount++;
 
     return Promise.resolve<void>();
   }
