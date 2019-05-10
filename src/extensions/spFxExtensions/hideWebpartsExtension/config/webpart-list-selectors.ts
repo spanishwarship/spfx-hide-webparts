0736be244479d7103b46c906029b0a7140cced20
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2019-05-10 10:08:12 -0500

    Updated backButon css selector

--- src/extensions/spFxExtensions/hideWebpartsExtension/config/webpart-list-selectors.ts
+++ src/extensions/spFxExtensions/hideWebpartsExtension/config/webpart-list-selectors.ts
@@ -1,6 +1,6 @@
 const classList = {
     clearButton: '.ms-SearchBox-clearButton',
-    backButton: 'button[id*="BackButton"]',
+    backButton: 'button[data-automation-id*="toolbox-back-button"]',
     searchBox: '.ms-SearchBox-field',
     seeAllButtons: "button[class*='seeAllButton_']",
     categoryLinks: '.ms-Dialog-main button[id*="CategoryContextualMenu"]',
